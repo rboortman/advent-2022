cb65c708c708533025b86fe176d9f0b547054759
Ron Boortman <ron.boortman@kpn.com>
2022-12-05 10:08:13 +0100

    Finishes day 5

---
+++ src/assignment_5.rs
@@ -0,0 +1,137 @@
+use regex::Regex;
+use std::collections::VecDeque;
+
+use crate::Assignment;
+
+#[derive(Debug, Clone)]
+pub struct Crate {
+    id: char,
+}
+
+impl Crate {
+    fn new(c: char) -> Crate {
+        Crate { id: c }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Instruction {
+    amount: usize,
+    from: usize,
+    to: usize,
+}
+
+impl Instruction {
+    fn new(amount: usize, from: usize, to: usize) -> Instruction {
+        Instruction { amount, from, to }
+    }
+}
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn new() -> Solution {
+        Solution {}
+    }
+}
+
+impl Assignment for Solution {
+    type Input = (VecDeque<VecDeque<Crate>>, VecDeque<Instruction>);
+    type Output = String;
+
+    fn parse_input(&self, input: &String) -> Option<Self::Input> {
+        let mut stacks = VecDeque::new();
+        let mut instructions = VecDeque::new();
+        let mut convert_stacks = true;
+        let total_stacks = (input.find('\n').unwrap() + 1) / 4;
+
+        let instruction_re =
+            Regex::new(r"^move (?P<amount>\d+) from (?P<from>\d+) to (?P<to>\d+)$").unwrap();
+
+        for _ in 0..total_stacks {
+            stacks.push_back(VecDeque::new());
+        }
+
+        for line in input.lines() {
+            if line == "" {
+                convert_stacks = false;
+                continue;
+            }
+
+            if convert_stacks {
+                for (index, chunks) in line.chars().collect::<Vec<char>>().chunks(4).enumerate() {
+                    if chunks[0] == '[' {
+                        stacks[index].push_front(Crate::new(chunks[1]));
+                    }
+                }
+            } else {
+                let cap = instruction_re.captures(line).unwrap();
+                instructions.push_back(Instruction::new(
+                    cap.name("amount").unwrap().as_str().parse().unwrap(),
+                    cap.name("from").unwrap().as_str().parse::<usize>().unwrap() - 1,
+                    cap.name("to").unwrap().as_str().parse::<usize>().unwrap() - 1,
+                ))
+            }
+        }
+
+        Some((stacks, instructions))
+    }
+
+    fn silver(&self, (s, instructions): &Self::Input) -> Option<Self::Output> {
+        let mut stacks = s.clone();
+
+        for instruction in instructions {
+            for _ in 0..instruction.amount {
+                let cr = stacks[instruction.from].pop_back().unwrap();
+                stacks[instruction.to].push_back(cr);
+            }
+        }
+
+        Some(
+            stacks
+                .iter()
+                .map(|stack| stack.back().unwrap().id)
+                .collect(),
+        )
+    }
+
+    fn gold(&self, (s, instructions): &Self::Input) -> Option<Self::Output> {
+        let mut stacks = s.clone();
+
+        for instruction in instructions {
+            let stack_length = stacks[instruction.from].len() - instruction.amount;
+            let mut crates = stacks[instruction.from].split_off(stack_length);
+            stacks[instruction.to].append(&mut crates);
+        }
+
+        Some(
+            stacks
+                .iter()
+                .map(|stack| stack.back().unwrap().id)
+                .collect(),
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static TEST_INPUT: &str = "    [D]    \n[N] [C]    \n[Z] [M] [P]\n 1   2   3 \n\nmove 1 from 2 to 1\nmove 3 from 1 to 3\nmove 2 from 2 to 1\nmove 1 from 1 to 2";
+
+    #[test]
+    fn test_silver() {
+        let sol = Solution::new();
+        let input = sol.parse_input(&TEST_INPUT.to_owned());
+        let result = sol.silver(&input.unwrap()).unwrap();
+        assert_eq!(result, String::from("CMZ"))
+    }
+
+    #[test]
+    fn test_gold() {
+        let sol = Solution::new();
+        let input = sol.parse_input(&TEST_INPUT.to_owned());
+        let result = sol.gold(&input.unwrap()).unwrap();
+        assert_eq!(result, String::from("MCD"))
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ mod assignment_1;
 mod assignment_2;
 mod assignment_3;
 mod assignment_4;
+mod assignment_5;
 
 use reqwest::header::{COOKIE, USER_AGENT};
 use std::{fmt::Display, time::Instant};
@@ -13,6 +14,7 @@ pub fn solve(day: u8) {
         2 => assignment_2::Solution::new().run(raw_input),
         3 => assignment_3::Solution::new().run(raw_input),
         4 => assignment_4::Solution::new().run(raw_input),
+        5 => assignment_5::Solution::new().run(raw_input),
         d => println!("Day {} has not been solved yet", d),
     }
 }
